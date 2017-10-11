4d771921484e2d55393b8c27eda9e001b1c7c648
Eric Fuller <info@glassmoonmedia.com>
2017-10-11 08:44:28 -0400

    Messages class.

---
+++ src/js/puzzle/messages.js
@@ -0,0 +1,22 @@
+class Messages {
+	static getMessageContainer() {
+		return document.querySelector('.puzzle__message-container');
+	}
+
+	static createMessageMarkup(message) {
+		return `<h1 class="puzzle__message">${message}</h1>`;
+	}
+
+	static createMessage(message) {
+		const messageContainer = Messages.getMessageContainer();
+
+		messageContainer.classList.add('visible');
+		messageContainer.innerHTML = Messages.createMessageMarkup(message);
+	}
+
+	static clearMessage() {
+		Messages.getMessageContainer().innerHTML = '';
+	}
+}
+
+export default Messages;
